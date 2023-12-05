2f596853b97abd01cf480581b9a2e22b641beef6
Aditya prajapati <132464789+adityaaa1702@users.noreply.github.com>
2023-12-05 18:46:22 +0530

    Add files via upload

--- statistic/forms.py
+++ statistic/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import UploadedFile
+import pandas as pd
 
 class UploadFileForm(forms.ModelForm):
     class Meta:
         model = UploadedFile
         fields = ['csv_file']
         
+   
--- statistic/models.py
+++ statistic/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+import os
 
 
 class UploadedFile(models.Model):
     csv_file = models.FileField(upload_to='uploads/')
+    
 
